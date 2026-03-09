325ae6d80f2f5648dda2cffda99e363873e3fbf9
LiHe <2301616558@qq.com>
2026-03-09 23:15:17 +0800

    公共字段自动填充代码实现

---
+++ sky-server/src/main/java/com/sky/acpect/AutoFillAspect.java
@@ -0,0 +1,94 @@
+package com.sky.acpect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+/**
+ * 切面类，用于处理自动填充功能字段
+ */
+
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /**
+     * 切入点
+     */@Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointcut() {
+        log.info("自动填充功能字段");
+    }
+
+    /**
+     * 前置通知，在目标方法执行前自动填充功能字段
+     * @param joinPoint 连接点对象，用于获取目标方法的信息
+     */
+    @Before("autoFillPointcut()")
+    public void autoFill(JoinPoint joinPoint) {
+         log.info("自动填充功能字段");
+         //接收当前被拦截的方法上的数据库操作类型
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();//获取方法签名
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);//获取方法上的 AutoFill 注解
+        OperationType operationType = autoFill.value();//获取注解上的操作类型
+
+        //获取当前被拦截方法的参数--实体对象
+        Object[] args=joinPoint.getArgs();
+        if (args==null || args.length==0) {
+            return;
+        }
+        //获取实体对象
+        Object entity = args[0];
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+        //根据数据库操作类型，为对应的属性赋值
+        if(operationType==OperationType.INSERT){
+         try {
+                Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                setCreateTime.setAccessible(true);
+                setCreateTime.invoke(entity, localDateTime);
+
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                setUpdateTime.setAccessible(true);
+                setUpdateTime.invoke(entity, localDateTime);
+
+                Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                setCreateUser.setAccessible(true);
+                setCreateUser.invoke(entity, currentId);
+
+                Method setUpdateUser= entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+                setUpdateUser.setAccessible(true);
+                setUpdateUser.invoke(entity, currentId);
+            }catch (Exception e) {
+                log.error("自动填充创建时间和更新时间失败", e);
+            }
+
+        }else if(operationType==OperationType.UPDATE){
+         try {
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                setUpdateTime.setAccessible(true);
+                setUpdateTime.invoke(entity, localDateTime);
+
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+                setUpdateUser.setAccessible(true);
+                setUpdateUser.invoke(entity, currentId);
+            }catch (Exception e) {
+                log.error("自动填充更新时间和更新人失败", e);
+            }
+        }
+    }
+
+}
+
+
---
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -0,0 +1,16 @@
+package com.sky.annotation;
+
+import com.sky.enumeration.OperationType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解，用于标识某个方法需要进行功能字段自动填充处理
+ */
+@Target(java.lang.annotation.ElementType.METHOD)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface AutoFill {
+    //指定数据库操作类型：INSERT或UPDATE
+    OperationType value();
+}
